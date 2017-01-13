91dfd2f53f8c087306ee7bc3b879d175c49a15e5
jisenw1 <jisenw1@WINX-06172.jbhunt.com>
2017-01-13 15:28:28 -0600

    Checking if it can be drawn inone area

--- PuzzleGame/src/Viz.java
+++ PuzzleGame/src/Viz.java
@@ -37,10 +37,10 @@ class View extends JPanel implements MouseListener {
 		int tempState = state[rand.nextInt(22)] + (rand.nextInt(2) == 0 ? -1 : 1);
 		
 		
-		checkValidStates(board); //Check valid states to make sure its a valid move
+		int id = 0;
+		checkValidStates(board, state[2*id],state[2*id+1]); //Check valid states to make sure its a valid move
 		
-		state[0] +=2;
-		state[1] += -3;
+		
 		
 		//state[3]+=-2;
 		
@@ -65,27 +65,47 @@ class View extends JPanel implements MouseListener {
 	}
 
 	// Draw a 3-block piece
-	public void shape(int id, int red, int green, int blue,
+	public boolean shape(int id, int red, int green, int blue,
 		int x1, int y1, int x2, int y2, int x3, int y3)
 	{
-		board[x1][y1] = true;
-		board[x2][y2] = true;
-		board[x3][y3] = true;
+		//Checking that they havent been drawn on, if not we can move the piece here and set all the locations to true
+		if(board[x1][y1] != true && board[x2][y2] != true && board[x3][y3] != true){
+			board[x1][y1] = true; 
+			board[x2][y2] = true;
+			board[x3][y3] = true;
+		}
+		else{
+			System.out.println("Couldn't draw one of three - add more debug");
+			return false;
+		}
 		graphics.setColor(new Color(red, green, blue));
 		b(state[2 * id] + x1, state[2 * id + 1] + y1);
 		b(state[2 * id] + x2, state[2 * id + 1] + y2);
 		b(state[2 * id] + x3, state[2 * id + 1] + y3);
-
+		return true;
 	}
 
 	// Draw a 4-block piece
-	public void shape(int id, int red, int green, int blue,
+	public boolean shape(int id, int red, int green, int blue,
 		int x1, int y1, int x2, int y2,
 		int x3, int y3, int x4, int y4)
 	{
-		board[x4][y4] = true;
-		shape(id, red, green, blue, x1, y1, x2, y2, x3, y3);
-		b(state[2 * id] + x4, state[2 * id + 1] + y4);
+		
+		//The only thing that changes when drawing, is the state[x][y] values. These get added to the x4, y4 coordinates
+		//When you change state[2*id], it will get added to x4 since x4 never changes. 
+		//That is to mean that to get the previous state of the block, you need to
+		if(board[x4][y4]==true){
+			System.out.println("Can't draw x4,y4 for " + id);
+			return false;
+		}
+		
+		else{
+			board[x4][y4] = true; //Set new state location. 
+			if(shape(id, red, green, blue, x1, y1, x2, y2, x3, y3))
+				System.out.println("Worked");
+			b(state[2 * id] + x4, state[2 * id + 1] + y4);
+		}
+		return true;
 	}
 
 	public void paintComponent(Graphics g)
@@ -115,8 +135,8 @@ class View extends JPanel implements MouseListener {
 		shape(10, 255, 128, 0, 5, 1, 6, 1, 5, 2); */
 	}
 	
-	public boolean checkValidStates(boolean board[][]){
-		
+	public boolean checkValidStates(boolean board[][], int x, int y){
+		//if()
 		
 		
 		
