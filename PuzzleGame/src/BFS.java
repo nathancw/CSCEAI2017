c2d7ef3e68211ff5fc5c60a56a939bc67bdb5dd0
NCW <nathanielcw@gmail.com>
2017-01-20 11:18:16 -0600

    Hook up that to a gui

---
+++ PuzzleGame/src/BFS.java
@@ -0,0 +1,398 @@
+import javax.swing.JFrame;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.Timer;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.awt.Graphics;
+import java.io.File;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Stack;
+import java.util.TreeSet;
+import java.awt.Color;
+import java.awt.Frame;
+
+class GameState
+{
+	GameState prev;
+	byte[] state;
+	boolean visited;
+	
+	public GameState(byte[] state2, GameState prev) {
+		this.prev = prev;
+		this.state = state2;
+	}
+
+	public void setVisited(boolean val){
+		this.visited = val;
+	}
+	
+	public boolean hasBeenVisited(){
+		return visited;
+	}
+	
+	public byte[] getState(){
+		return state;
+	}
+	
+	public void print(){
+		for(int i = 0; i < 11; i++)
+			System.out.print("(" + this.state[2 * i] + "," +
+					this.state[2 * i + 1] + ") ");
+				System.out.println();
+
+	}
+/*	GameState(GameState _prev)
+	{
+		prev = _prev;
+		state = new byte[22];
+	}
+*/
+}
+
+class StateComparator implements Comparator<GameState>
+{
+	public int compare(GameState a, GameState b)
+	{
+		for(int i = 0; i < 22; i++)
+		{
+			if(a.state[i] < b.state[i])
+				return -1;
+			else if(a.state[i] > b.state[i])
+				return 1;
+		}
+		return 0;
+	}
+}  
+
+
+class Search implements MouseListener {
+	BFS viz;
+	Random rand;
+	byte[] state;
+	Graphics graphics;
+	int size;
+	boolean[][] board;
+	Queue<GameState> queue;
+	StateComparator comp;
+	TreeSet<GameState> set;
+	Stack solution;
+
+	class Node {
+		   char data;
+		   public Node(char c) {
+		      this.data=c;
+		   }
+		}
+	
+	Search(BFS v) throws IOException
+	{
+		viz = v;
+		rand = new Random();
+		state = new byte[22];
+		size = 48;
+		board = new boolean[10][10];
+		queue = new LinkedList<GameState>();
+		comp = new StateComparator();
+		set = new TreeSet<GameState>(comp);
+		solution = new Stack();
+	}
+	
+	public void bfs(){
+		
+		// BFS uses Queue data structure
+		//graphics = null;
+		GameState root = new GameState(state, null);
+		queue.add(root);
+		root.setVisited(true);
+		
+		//set.add(root);
+		
+		//if(set.contains(root))
+		//	System.out.println("THE SET CONTAINS THE ROOT");
+		
+		
+		byte[] objective = state.clone();
+		
+		int solutionID = 7;
+		byte x = 0;
+		byte y = -3;
+		
+		
+		objective[solutionID*2]=x;
+		objective[solutionID*2+1]=y;
+		GameState goal = null;
+		
+		while(!queue.isEmpty()) {
+			GameState node = queue.remove();
+			GameState child = null;
+			
+			if(node.getState()[solutionID*2]==objective[solutionID*2] && node.getState()[solutionID*2+1]==objective[solutionID*2+1]){
+				System.out.println("found objective");
+				goal = node;
+				break;
+			}
+			else{
+			
+				//Populat the queue with each adjacent nodes. 
+				for(int index = 0; index < 11; index++){
+					
+					moveLeft(index, node);
+					moveRight(index, node);
+					moveUp(index, node);
+					moveDown(index, node);
+				}
+			}
+		
+		}  
+		
+		//Go backwards and print the trail
+		GameState current = goal;
+		//current.print();
+		int moveCount = -1;
+		while(current!=null){
+			moveCount++;
+			solution.add(current.getState());
+			current.print();
+			current = current.prev;
+		}
+		System.out.println("Move Count : " + moveCount);
+		
+	}
+	
+	public Stack getStack(){
+		return solution;
+	}
+	
+	public void mousePressed(MouseEvent e)
+	{
+	
+		
+
+	}
+
+	public boolean moveDown(int id, GameState root) {
+		byte[] state = root.getState().clone();
+		state[2*id+1] = (byte) (state[2*id+1] + 1);
+		boolean validMove = drawShapes(id, state);
+		
+		if(validMove){
+			if(!set.contains(new GameState(state, root)))
+			queue.add(new GameState(state, root)); //add the game state
+			return true;
+		}
+		else{
+			//state[2*id+1] = (byte) (state[2*id+1] - 1);
+			return false;
+		}
+	}
+
+	public boolean moveUp(int id, GameState root) {
+		byte[] state = root.getState().clone();
+		state[2*id+1] = (byte) (state[2*id+1] - 1);
+		boolean validMove = drawShapes(id, state);
+		
+		if(validMove){
+			if(!set.contains(new GameState(state, root)))
+			queue.add(new GameState(state, root)); //add the game state
+			return true;
+		}
+		else{
+			//state[2*id+1] = (byte) (state[2*id+1] + 1);
+			return false;
+		}
+	}
+
+	public boolean moveRight(int id, GameState root) {
+		
+		byte[] state = root.getState().clone();
+		
+		state[2*id] = (byte) (state[2*id] + 1);
+		boolean validMove = drawShapes(id, state);
+		
+		if(validMove){
+			if(!set.contains(new GameState(state, root)))
+			queue.add(new GameState(state, root)); //add the game state
+			return true;
+		}
+		else{
+			//state[2*id] = (byte) (state[2*id] - 1);
+			return false;
+		}
+
+	}
+
+	public boolean moveLeft(int id, GameState root) {
+		
+		byte[] state = root.getState().clone();
+		
+		state[2*id] = (byte) (state[2*id] - 1);
+		boolean validMove = drawShapes(id, state);
+		
+		if(validMove){
+			if(!set.contains(new GameState(state, root))){
+				//System.out.println("Valid move");
+				queue.add(new GameState(state, root)); //add the game state
+			}
+			return true;
+		}
+		else{
+			//state[2*id] = (byte) (state[2*id] + 1);
+			return false;
+		}
+	
+	}
+
+	public boolean drawShapes(int id, byte[] state) {
+	
+		for(int x = 0; x < 10; x++)
+			for(int c = 0; c < 10; c++)
+				board[x][c] = false;
+		
+		for(int i = 0; i < 10; i++) { b(i, 0); b(i, 9); }
+		for(int i = 1; i < 9; i++) { b(0, i); b(9, i); }
+		b(1, 1); b(1, 2); b(2, 1);
+		b(7, 1); b(8, 1); b(8, 2);
+		b(1, 7); b(1, 8); b(2, 8);
+		b(8, 7); b(7, 8); b(8, 8);
+		b(3, 4); b(4, 4); b(4, 3);
+		
+		
+		// Draw the pieces
+		if(!shape(0, 255, 0, 0, 1, 3, 2, 3, 1, 4, 2, 4, state))
+				return false;
+		else if(!shape(1, 0, 255, 0, 1, 5, 1, 6, 2, 6, state))
+			return false;
+		else if(!shape(2, 128, 128, 255, 2, 5, 3, 5, 3, 6, state))
+				return false;
+		else if(!shape(3, 255, 128, 128, 3, 7, 3, 8, 4, 8, state))
+				return false;
+		else if(!shape(4, 255, 255, 128, 4, 7, 5, 7, 5, 8, state))
+				return false;
+		else if(!shape(5, 128, 128, 0, 6, 7, 7, 7, 6, 8, state))
+				return false;
+		else if(!shape(6, 0, 128, 128, 5, 4, 5, 5, 5, 6, 4, 5, state))
+				return false;
+		else if(!shape(7, 0, 128, 0, 6, 4, 6, 5, 6, 6, 7, 5, state))
+				return false;
+		else if(!shape(8, 0, 255, 255, 8, 5, 8, 6, 7, 6,state)) 
+				return false; 
+		else if(!shape(9, 0, 0, 255, 6, 2, 6, 3, 5, 3,state))
+				return false; 
+		else if(!shape(10, 255, 128, 0, 5, 1, 6, 1, 5, 2, state)){
+			return false;
+		}
+		else
+			return true;
+		
+		
+	}
+
+	public void mouseReleased(MouseEvent e) {    }
+	public void mouseEntered(MouseEvent e) {    }
+	public void mouseExited(MouseEvent e) {    }
+	public void mouseClicked(MouseEvent e) {    }
+
+	//B checks valid states
+	public boolean b(int x, int y)
+	{
+		//if(x < 10 && y < 10)
+			if(board[x][y] == true){
+			//	System.out.println("Found error at : " + x + "," + y);
+				return false;
+			}
+		
+		board[x][y] = true;
+		//graphics.fillRect(size * x, size * y, size, size);
+			return true;
+	}
+
+	// Draw a 3-block piece
+	public boolean shape(int id, int red, int green, int blue,
+		int x1, int y1, int x2, int y2, int x3, int y3, byte[] state)
+	{
+		//graphics.setColor(new Color(red, green, blue));
+		boolean b1 = b(state[2 * id] + x1, state[2 * id + 1] + y1);
+		boolean b2 = b(state[2 * id] + x2, state[2 * id + 1] + y2);
+		boolean b3 = b(state[2 * id] + x3, state[2 * id + 1] + y3);
+		//System.out.println("b1 :  " + b1 + " b2: " + b2 + " b3 : " + b3 + " total: " + (b1 && b2 && b3));
+		return (b1 && b2 && b3);
+	}
+
+	// Draw a 4-block piece
+	public boolean shape(int id, int red, int green, int blue,
+		int x1, int y1, int x2, int y2,
+		int x3, int y3, int x4, int y4, byte[] state)
+	{
+		boolean s1 = shape(id, red, green, blue, x1, y1, x2, y2, x3, y3, state);
+		boolean b1 = b(state[2 * id] + x4, state[2 * id + 1] + y4);
+		
+		return (s1 && b1);
+	}
+
+	public void paintComponent(Graphics g)
+	{
+		for(int x = 0; x < 10; x++)
+			for(int c = 0; c < 10; c++)
+				board[x][c] = false;
+	
+		
+		// Draw the black squares
+		graphics = g;
+		g.setColor(new Color(0, 0, 0));
+		for(int i = 0; i < 10; i++) { b(i, 0); b(i, 9); }
+		for(int i = 1; i < 9; i++) { b(0, i); b(9, i); }
+		b(1, 1); b(1, 2); b(2, 1);
+		b(7, 1); b(8, 1); b(8, 2);
+		b(1, 7); b(1, 8); b(2, 8);
+		b(8, 7); b(7, 8); b(8, 8);
+		b(3, 4); b(4, 4); b(4, 3);
+
+		// Draw the pieces
+		shape(0, 255, 0, 0, 1, 3, 2, 3, 1, 4, 2, 4,state);
+		shape(1, 0, 255, 0, 1, 5, 1, 6, 2, 6,state);
+		shape(2, 128, 128, 255, 2, 5, 3, 5, 3, 6,state);
+		shape(3, 255, 128, 128, 3, 7, 3, 8, 4, 8,state);
+		shape(4, 255, 255, 128, 4, 7, 5, 7, 5, 8,state);
+		shape(5, 128, 128, 0, 6, 7, 7, 7, 6, 8,state);
+		shape(6, 0, 128, 128, 5, 4, 5, 5, 5, 6, 4, 5,state);
+		shape(7, 0, 128, 0, 6, 4, 6, 5, 6, 6, 7, 5,state);
+		shape(8, 0, 255, 255, 8, 5, 8, 6, 7, 6,state); 
+		shape(9, 0, 0, 255, 6, 2, 6, 3, 5, 3,state); 
+		shape(10, 255, 128, 0, 5, 1, 6, 1, 5, 2,state); 
+	}
+
+	
+}
+
+public class BFS extends JFrame
+{
+	Search search;
+	public BFS() throws Exception
+	{
+		search = new Search(this);
+		search.bfs();
+		/*view.addMouseListener(view);
+		this.setTitle("Puzzle");
+		this.setSize(482, 505);
+		this.getContentPane().add(view);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true); */
+	}
+	
+	public Stack getStack(){
+		return search.getStack();
+	}
+	public static void main(String[] args) throws Exception
+	{
+		new BFS();
+	}
+}
--- PuzzleGame/src/Viz.java
+++ PuzzleGame/src/Viz.java
@@ -11,263 +11,40 @@ import java.awt.Graphics;
 import java.io.File;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Random;
+import java.util.Stack;
 import java.awt.Color;
-
-class GameState
-{
-	///GameState prev;
-	byte[] state;
-	boolean visited;
-	
-	public GameState(byte[] state2) {
-		this.state = state2;
-	}
-
-	public void setVisited(boolean val){
-		this.visited = val;
-	}
-	
-	public boolean hasBeenVisited(){
-		return visited;
-	}
-	
-	public byte[] getState(){
-		return state;
-	}
-	
-	public void print(){
-		for(int i = 0; i < 11; i++)
-			System.out.print("(" + this.state[2 * i] + "," +
-					this.state[2 * i + 1] + ") ");
-				System.out.println();
-
-	}
-/*	GameState(GameState _prev)
-	{
-		prev = _prev;
-		state = new byte[22];
-	}
-*/
-}
-
-class StateComparator implements Comparator<GameState>
-{
-	public int compare(GameState a, GameState b)
-	{
-		for(int i = 0; i < 22; i++)
-		{
-			if(a.state[i] < b.state[i])
-				return -1;
-			else if(a.state[i] > b.state[i])
-				return 1;
-		}
-		return 0;
-	}
-}  
-
 
 class View extends JPanel implements MouseListener {
 	Viz viz;
+	BFS game;
+	Stack solution;
 	Random rand;
 	byte[] state;
 	Graphics graphics;
 	int size;
-	boolean[][] board;
-	Queue<GameState> queue;
 
-	class Node {
-		   char data;
-		   public Node(char c) {
-		      this.data=c;
-		   }
-		}
-	
 	View(Viz v) throws IOException
 	{
+		try {
+			game = new BFS();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		solution = game.getStack();
 		viz = v;
-		rand = new Random();
+		rand = new Random(0);
 		state = new byte[22];
 		size = 48;
-		board = new boolean[10][10];
-		queue = new LinkedList<GameState>();
 	}
-	
-	public void bfs(){
-		
-		// BFS uses Queue data structure
-		
-		GameState root = new GameState(state);
-		queue.add(root);
-		root.setVisited(true);
-		
-		boolean validMove = false;
-		
-		for(int index = 0; index < 11; index++){
-			
-			moveLeft(index, root);
-			moveRight(index, root);
-			moveUp(index, root);
-			moveDown(index, root);
-		}
-		
-		/*while(!queue.isEmpty()) {
-			GameState node = queue.remove();
-			GameState child = null;
-			
-			//STOPPED HERE
-			while((child=getUnvisitedChildNode(node))!=null) {
-				child.visited=true;
-				printNode(child);
-				queue.add(child);
-			}
-		} */
-		// Clear visited property of nodes
-		//clearNodes();
-		
-		
-	}
-	
+
 	public void mousePressed(MouseEvent e)
 	{
-		/*
-		boolean validMove;
 		
-		//Looping through all the shapes and seeing if it can move in that direction
-		for(int index = 0; index < 11; index++){
-			
-			validMove = moveLeft(index);
-			if(validMove)
-				viz.repaint(); 
-			
-			validMove = moveRight(index);
-			if(validMove)
-				viz.repaint();
-			
-			validMove = moveUp(index);
-			if(validMove)
-				viz.repaint(); 
-			
-			validMove = moveDown(index);
-			if(validMove)
-				viz.repaint();
-		}
-	*/
+		if(!solution.isEmpty())
+			state = (byte[]) solution.pop();
 		
-	}
-
-	public boolean moveDown(int id, GameState root) {
-		byte[] state = root.getState().clone();
-		state[2*id+1] = (byte) (state[2*id+1] + 1);
-		boolean validMove = drawShapes(id, state);
-		
-		if(validMove){
-			queue.add(new GameState(state)); //add the game state
-			return true;
-		}
-		else{
-			//state[2*id+1] = (byte) (state[2*id+1] - 1);
-			return false;
-		}
-	}
-
-	public boolean moveUp(int id, GameState root) {
-		byte[] state = root.getState().clone();
-		state[2*id+1] = (byte) (state[2*id+1] - 1);
-		boolean validMove = drawShapes(id, state);
-		
-		if(validMove){
-			queue.add(new GameState(state)); //add the game state
-			return true;
-		}
-		else{
-			//state[2*id+1] = (byte) (state[2*id+1] + 1);
-			return false;
-		}
-	}
-
-	public boolean moveRight(int id, GameState root) {
-		
-		byte[] state = root.getState().clone();
-		
-		state[2*id] = (byte) (state[2*id] + 1);
-		boolean validMove = drawShapes(id, state);
-		
-		if(validMove){
-			queue.add(new GameState(state)); //add the game state
-			return true;
-		}
-		else{
-			//state[2*id] = (byte) (state[2*id] - 1);
-			return false;
-		}
-
-	}
-
-	public boolean moveLeft(int id, GameState root) {
-		
-		byte[] state = root.getState().clone();
-		
-		state[2*id] = (byte) (state[2*id] - 1);
-		boolean validMove = drawShapes(id, state);
-		
-		if(validMove){
-			queue.add(new GameState(state)); //add the game state
-			return true;
-		}
-		else{
-			//state[2*id] = (byte) (state[2*id] + 1);
-			return false;
-		}
-	
-	}
-
-	public boolean drawShapes(int id, byte[] state) {
-	
-		for(int x = 0; x < 10; x++)
-			for(int c = 0; c < 10; c++)
-				board[x][c] = false;
-		
-		for(int i = 0; i < 10; i++) { b(i, 0); b(i, 9); }
-		for(int i = 1; i < 9; i++) { b(0, i); b(9, i); }
-		b(1, 1); b(1, 2); b(2, 1);
-		b(7, 1); b(8, 1); b(8, 2);
-		b(1, 7); b(1, 8); b(2, 8);
-		b(8, 7); b(7, 8); b(8, 8);
-		b(3, 4); b(4, 4); b(4, 3);
-		
-		
-		// Draw the pieces
-		if(!shape(0, 255, 0, 0, 1, 3, 2, 3, 1, 4, 2, 4, state))
-				return false;
-		else if(!shape(1, 0, 255, 0, 1, 5, 1, 6, 2, 6, state))
-			return false;
-		else if(!shape(2, 128, 128, 255, 2, 5, 3, 5, 3, 6, state))
-				return false;
-		else if(!shape(3, 255, 128, 128, 3, 7, 3, 8, 4, 8, state))
-				return false;
-		else if(!shape(4, 255, 255, 128, 4, 7, 5, 7, 5, 8, state))
-				return false;
-		else if(!shape(5, 128, 128, 0, 6, 7, 7, 7, 6, 8, state))
-				return false;
-		else if(!shape(6, 0, 128, 128, 5, 4, 5, 5, 5, 6, 4, 5, state))
-				return false;
-		else if(!shape(7, 0, 128, 0, 6, 4, 6, 5, 6, 6, 7, 5, state))
-				return false;
-		else if(!shape(8, 0, 255, 255, 8, 5, 8, 6, 7, 6,state)) 
-				return false; 
-		else if(!shape(9, 0, 0, 255, 6, 2, 6, 3, 5, 3,state))
-				return false; 
-		else if(!shape(10, 255, 128, 0, 5, 1, 6, 1, 5, 2, state)){
-			return false;
-		}
-		else
-			return true;
-		
-		
+		viz.repaint();
 	}
 
 	public void mouseReleased(MouseEvent e) {    }
@@ -275,50 +52,33 @@ class View extends JPanel implements MouseListener {
 	public void mouseExited(MouseEvent e) {    }
 	public void mouseClicked(MouseEvent e) {    }
 
-	//B checks valid states
-	public boolean b(int x, int y)
+	// Draw a block
+	public void b(int x, int y)
 	{
-		//if(x < 10 && y < 10)
-			if(board[x][y] == true){
-			//	System.out.println("Found error at : " + x + "," + y);
-				return false;
-			}
-		
-		board[x][y] = true;
-		//graphics.fillRect(size * x, size * y, size, size);
-			return true;
+		graphics.fillRect(size * x, size * y, size, size);
 	}
 
 	// Draw a 3-block piece
-	public boolean shape(int id, int red, int green, int blue,
-		int x1, int y1, int x2, int y2, int x3, int y3, byte[] state)
+	public void shape(int id, int red, int green, int blue,
+		int x1, int y1, int x2, int y2, int x3, int y3)
 	{
-		//graphics.setColor(new Color(red, green, blue));
-		boolean b1 = b(state[2 * id] + x1, state[2 * id + 1] + y1);
-		boolean b2 = b(state[2 * id] + x2, state[2 * id + 1] + y2);
-		boolean b3 = b(state[2 * id] + x3, state[2 * id + 1] + y3);
-		//System.out.println("b1 :  " + b1 + " b2: " + b2 + " b3 : " + b3 + " total: " + (b1 && b2 && b3));
-		return (b1 && b2 && b3);
+		graphics.setColor(new Color(red, green, blue));
+		b(state[2 * id] + x1, state[2 * id + 1] + y1);
+		b(state[2 * id] + x2, state[2 * id + 1] + y2);
+		b(state[2 * id] + x3, state[2 * id + 1] + y3);
 	}
 
 	// Draw a 4-block piece
-	public boolean shape(int id, int red, int green, int blue,
+	public void shape(int id, int red, int green, int blue,
 		int x1, int y1, int x2, int y2,
-		int x3, int y3, int x4, int y4, byte[] state)
+		int x3, int y3, int x4, int y4)
 	{
-		boolean s1 = shape(id, red, green, blue, x1, y1, x2, y2, x3, y3, state);
-		boolean b1 = b(state[2 * id] + x4, state[2 * id + 1] + y4);
-		
-		return (s1 && b1);
+		shape(id, red, green, blue, x1, y1, x2, y2, x3, y3);
+		b(state[2 * id] + x4, state[2 * id + 1] + y4);
 	}
 
 	public void paintComponent(Graphics g)
 	{
-		/*for(int x = 0; x < 10; x++)
-			for(int c = 0; c < 10; c++)
-				board[x][c] = false;
-	
-		
 		// Draw the black squares
 		graphics = g;
 		g.setColor(new Color(0, 0, 0));
@@ -339,12 +99,10 @@ class View extends JPanel implements MouseListener {
 		shape(5, 128, 128, 0, 6, 7, 7, 7, 6, 8);
 		shape(6, 0, 128, 128, 5, 4, 5, 5, 5, 6, 4, 5);
 		shape(7, 0, 128, 0, 6, 4, 6, 5, 6, 6, 7, 5);
-		shape(8, 0, 255, 255, 8, 5, 8, 6, 7, 6); 
-		shape(9, 0, 0, 255, 6, 2, 6, 3, 5, 3); 
-		shape(10, 255, 128, 0, 5, 1, 6, 1, 5, 2); */
+		shape(8, 0, 255, 255, 8, 5, 8, 6, 7, 6);
+		shape(9, 0, 0, 255, 6, 2, 6, 3, 5, 3);
+		shape(10, 255, 128, 0, 5, 1, 6, 1, 5, 2);
 	}
-
-	
 }
 
 public class Viz extends JFrame
@@ -352,13 +110,12 @@ public class Viz extends JFrame
 	public Viz() throws Exception
 	{
 		View view = new View(this);
-		view.bfs();
-		/*view.addMouseListener(view);
+		view.addMouseListener(view);
 		this.setTitle("Puzzle");
 		this.setSize(482, 505);
 		this.getContentPane().add(view);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setVisible(true); */
+		this.setVisible(true);
 	}
 
 	public static void main(String[] args) throws Exception
