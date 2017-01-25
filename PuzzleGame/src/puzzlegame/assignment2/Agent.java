4c886e9ab9f467f2b2bd2b5b67ee4c88c62a3dac
NCW <nathanielcw@gmail.com>
2017-01-25 13:52:24 -0600

    added a2

---
+++ PuzzleGame/src/puzzlegame/assignment2/Agent.java
@@ -0,0 +1,30 @@
+package puzzlegame.assignment2;
+import java.util.ArrayList;
+import java.awt.event.MouseEvent;
+import java.awt.Graphics;
+import java.awt.Color;
+
+class Agent {
+
+	void drawPlan(Graphics g, Model m) {
+		g.setColor(Color.red);
+		g.drawLine((int)m.getX(), (int)m.getY(), (int)m.getDestinationX(), (int)m.getDestinationY());
+	}
+
+	void update(Model m)
+	{
+		Controller c = m.getController();
+		while(true)
+		{
+			MouseEvent e = c.nextMouseEvent();
+			if(e == null)
+				break;
+			m.setDestination(e.getX(), e.getY());
+		}
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		Controller.playGame();
+	}
+}
