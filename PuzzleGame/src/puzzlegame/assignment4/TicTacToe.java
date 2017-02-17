94d880af5626ee15a45dc0a5ceee648843509947
nathancw <nathanielcw@gmail.com>
2017-02-17 15:48:23 -0600

    0

--- PuzzleGame/src/puzzlegame/assignment4/TicTacToe.java
+++ PuzzleGame/src/puzzlegame/assignment4/TicTacToe.java
@@ -1,49 +1,46 @@
 package puzzlegame.assignment4;
 
 import java.util.List;
+import java.util.Queue;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 
-class Node<T> {
-    private List<Node<T>> children = new ArrayList<Node<T>>();
-    private Node<T> parent = null;
-    private T data = null;
+class Node {
+    List<Node> children = new ArrayList<Node>();
+    Node parent = null;
+    char[] data = null;
 
-    public Node(T data) {
+    public Node(char[] data) {
         this.data = data;
     }
 
-    public Node(T data, Node<T> parent) {
+    public Node(char[] data, Node parent) {
         this.data = data;
         this.parent = parent;
     }
 
-    public List<Node<T>> getChildren() {
+    public List<Node> getChildren() {
         return children;
     }
 
-    public void setParent(Node<T> parent) {
+    public void setParent(Node parent) {
         parent.addChild(this);
         this.parent = parent;
     }
 
-    public void addChild(T data) {
-        Node<T> child = new Node<T>(data);
+    public void addChild(char[] data) {
+        Node child = new Node(data);
         child.setParent(this);
         this.children.add(child);
     }
 
-    public void addChild(Node<T> child) {
-        child.setParent(this);
+    public void addChild(Node child) {
+     //   child.setParent(this);
         this.children.add(child);
     }
 
-    public T getData() {
-        return this.data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
+ 
 
     public boolean isRoot() {
         return (this.parent == null);
@@ -65,11 +62,14 @@ class Node<T> {
 
 public class TicTacToe {
 	Node root;
+	Queue queue;
 	public TicTacToe(){
 		char[] board = {'1','2','3','4','5','6','7','8','9'};
 		root = new Node(board);
-		calculateBoardStates(root);
-		drawBoard(board);
+		queue = new LinkedList<Node>();
+		queue.add(root);
+		calculateBoardStates(root,'X');
+		//drawBoard(board);
 	}
 	
 	public void drawBoard(char[] board){
@@ -82,9 +82,61 @@ public class TicTacToe {
 		
 	}
 	
-	public void calculateBoardStates(Node root){
+	public void calculateBoardStates(Node root, char player){
 		
+		while(!queue.isEmpty()){
+			Node current = (Node) queue.remove();
+			addState(1,player,current);
+			addState(2,player, current);
+			addState(3,player, current);
+			addState(4,player, current);
+			addState(5,player, current);
+			addState(6,player, current);
+			addState(7,player, current);
+			addState(8,player, current);
+			addState(9,player, current);
+			
+			if(player=='X')
+				player = 'O';
+			else if(player=='O')
+				player = 'X';
+		}
 		
+		//for(int x = 0; x < 9; x++){
+		Node curr = root;
+		int depth = 0;
+		while(curr.children.size()!=0){	
+			drawBoard(curr.children.get(0).data);
+			curr = curr.children.get(0);
+			depth++;
+			System.out.println("\n " + depth + "\n");
+			
+		}
+		
+	}
+	
+	public void addState(int x, char player, Node root){
+		
+		char[] boardCopy = root.data.clone();
+		char c = root.data[x-1];
+		
+		//System.out.println("Adding: " + x  + " player: " + player);
+		
+		if(!gameCompleted(boardCopy))
+			if(c != 'X' && c!='O'){
+				boardCopy[x-1] = player;
+				Node node = new Node(boardCopy, root);
+				queue.add(node);
+				root.addChild(node);
+			}
+	}
+	
+	public boolean gameCompleted(char[] board){
+		for(int x = 0; x < 9; x++)
+			if(board[x] != 'X' || board[x] !='O')
+				return false;
+		
+		return true;
 	}
 	
 	public static void main(String[] args){
