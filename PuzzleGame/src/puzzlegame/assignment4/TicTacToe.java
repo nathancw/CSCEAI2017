a9acc6dce7d5b96a51c2bbef1b6de04258871178
NCW <nathanielcw@gmail.com>
2017-02-17 09:30:41 -0600

    k

--- PuzzleGame/src/puzzlegame/assignment4/TicTacToe.java
+++ PuzzleGame/src/puzzlegame/assignment4/TicTacToe.java
@@ -1,25 +1,78 @@
 package puzzlegame.assignment4;
 
-public class TicTacToe {
+import java.util.List;
+import java.util.ArrayList;
 
-	
-	public static void main(String[] args){
-		System.out.println("Playing tictactoe");
-		
+class Node<T> {
+    private List<Node<T>> children = new ArrayList<Node<T>>();
+    private Node<T> parent = null;
+    private T data = null;
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public Node(T data, Node<T> parent) {
+        this.data = data;
+        this.parent = parent;
+    }
+
+    public List<Node<T>> getChildren() {
+        return children;
+    }
+
+    public void setParent(Node<T> parent) {
+        parent.addChild(this);
+        this.parent = parent;
+    }
+
+    public void addChild(T data) {
+        Node<T> child = new Node<T>(data);
+        child.setParent(this);
+        this.children.add(child);
+    }
+
+    public void addChild(Node<T> child) {
+        child.setParent(this);
+        this.children.add(child);
+    }
+
+    public T getData() {
+        return this.data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public boolean isRoot() {
+        return (this.parent == null);
+    }
+
+    public boolean isLeaf() {
+        if(this.children.size() == 0) 
+            return true;
+        else 
+            return false;
+    }
+
+    public void removeParent() {
+        this.parent = null;
+    }
+}
+
+
+
+public class TicTacToe {
+	Node root;
+	public TicTacToe(){
 		char[] board = {'1','2','3','4','5','6','7','8','9'};
-		
+		root = new Node(board);
+		calculateBoardStates(root);
 		drawBoard(board);
-		/*System.out.println(" 1 | 2 | 3 ");
-		System.out.println("---+---+---");
-		System.out.println(" 4 | 5 | 6 ");
-		System.out.println("---+---+---");
-		System.out.println(" 7 | 8 | 9 ");
-		*/
-		
 	}
 	
-	
-	public static void drawBoard(char[] board){
+	public void drawBoard(char[] board){
 		
 		System.out.println(" " + board[0] + " | " + board[1] + " | " + board[2] + " ");
 		System.out.println("---+---+---");
@@ -28,4 +81,16 @@ public class TicTacToe {
 		System.out.println(" " + board[6] + " | " + board[7] + " | " + board[8] + " ");
 		
 	}
+	
+	public void calculateBoardStates(Node root){
+		
+		
+	}
+	
+	public static void main(String[] args){
+		System.out.println("Playing tictactoe");
+		TicTacToe t = new TicTacToe();
+		
+	}
 }
+
